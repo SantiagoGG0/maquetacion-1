e0bb503c6d4502c73fc879ee1177c2ff3c6fe2de
SantiagoGG0 <santiagogarcia337@gmail.com>
2023-03-30 21:24:48 -0500

    APLICACION

---
+++ app.js
@@ -0,0 +1,7 @@
+const menuBtn = document.getElementById("menuBtn");
+const aside = document.querySelector("aside");
+
+
+menuBtn.addEventListener("click", function(){
+aside.classList.toggle('active');
+})
